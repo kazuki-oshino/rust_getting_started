9b0366c40aef4eb4339c40938172fc558101fc02
kazuki-oshino <slash0juice@gmail.com>
2024-01-02 20:09:46 +0900

    add variables

---
+++ variables/target/debug/deps/variables-0a6516bc07ae3636.d
@@ -0,0 +1,5 @@
+/Users/kazuki/dev/getting_started/variables/target/debug/deps/variables-0a6516bc07ae3636: src/main.rs
+
+/Users/kazuki/dev/getting_started/variables/target/debug/deps/variables-0a6516bc07ae3636.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/kazuki/dev/getting_started/variables/target/debug/variables: /Users/kazuki/dev/getting_started/variables/src/main.rs
