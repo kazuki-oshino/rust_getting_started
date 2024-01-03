324cc8ec3d731844294d36d07af183cd73804c95
kazuki-oshino <slash0juice@gmail.com>
2024-01-03 17:21:31 +0900

    add data_type

---
+++ data_type/target/debug/data_type.d
@@ -0,0 +1,1 @@
+/Users/kazuki/dev/getting_started/data_type/target/debug/data_type: /Users/kazuki/dev/getting_started/data_type/src/main.rs
---
+++ data_type/target/debug/deps/data_type-e7865ac4552810ca.d
@@ -0,0 +1,5 @@
+/Users/kazuki/dev/getting_started/data_type/target/debug/deps/data_type-e7865ac4552810ca: src/main.rs
+
+/Users/kazuki/dev/getting_started/data_type/target/debug/deps/data_type-e7865ac4552810ca.d: src/main.rs
+
+src/main.rs:
