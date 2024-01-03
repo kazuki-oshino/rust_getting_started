b52c32eaeeb481cba61891fca8c2f8060ff36054
kazuki-oshino <slash0juice@gmail.com>
2024-01-03 18:06:04 +0900

    add data_type

--- data_type/src/main.rs
+++ data_type/src/main.rs
@@ -1,3 +1,4 @@
+use std::io;
 fn main() {
     let sum = 5 + 10;
     println!("sum: {}", sum);
@@ -18,4 +19,17 @@ fn main() {
     println!("five_hundred: {}", five_hundred);
     let a = [1, 2, 3, 4, 5];
     println!("a: {:?}", a);
+
+    println!("Please enter an array index.");
+    let mut index = String::new();
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+    let element = a[index];
+    println!("The value of the element at index {} is: {}", index, element);
+
 }
