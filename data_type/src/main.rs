8308c3f4754f60ef181341ace5da417f10698019
kazuki-oshino <slash0juice@gmail.com>
2024-01-03 18:02:06 +0900

    add data_type

--- data_type/src/main.rs
+++ data_type/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let sum = 5 + 10;
+    println!("sum: {}", sum);
+    let difference = 95.5 - 4.3;
+    println!("difference: {}", difference);
+    let product = 4 * 30;
+    println!("product: {}", product);
+    let quotient = 56.7 / 32.2;
+    println!("quotient: {}", quotient);
+    let floored = 2 / 3;
+    println!("floored: {}", floored);
+    let remainder = 43 % 5;
+    println!("remainder: {}", remainder);
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    println!("tup: {:?}", tup);
+    let five_hundred = tup.0;
+    println!("five_hundred: {}", five_hundred);
+    let a = [1, 2, 3, 4, 5];
+    println!("a: {:?}", a);
 }
